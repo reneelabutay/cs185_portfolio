0e72ac9c5fc28a0ea50f3bd201b5c2c5e749ae5f
reneelabutay <reneelabutay@Renees-MacBook-Pro.local>
2020-04-20 00:41:01 -0700

    completed assignment 3

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function enlargeImage() {
+//"Back to Top" Button
+$(document).ready(function() {
+            
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > $(window).height()*.25) {
+                    $('.go-to-top').fadeIn();
+                } else {
+                    $('.go-to-top').fadeOut();
+                }
+            });
 
-}
+            
+            $('.go-to-top').click(function(event) {
+                event.preventDefault();
+
+                $('html, body').animate({scrollTop: 0}, 300);
+            })
+        });
+
+/*Used this as a reference: https://stackoverflow.com/questions
+/22361901/sticky-back-to-top-button-showing-on-page-load-before-scrolling-down*/
